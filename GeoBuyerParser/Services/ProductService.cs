f86620f8f8010a1566212e9172151f532eb3d7b3
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2023-09-01 05:42:15 -0400

    restore

---
+++ GeoBuyerParser/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿
+
+using GeoBuyerParser.Models;
+using GeoBuyerParser.Repositories;
+
+namespace GeoBuyerParser.Services;
+
+public class ProductService
+{
+    public Repository Repository { get; }
+
+    public ProductService(Repository repository)
+    {
+        Repository = repository;
+    }
+
+    public List<ExtendedProduct> GetProducts()
+    {
+        return Repository.GetProducts();
+    }
+}
